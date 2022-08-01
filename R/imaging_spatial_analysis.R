4582c4ec8574229ee9329956da55b0a1a0a6262b
Sunwoo Han <sunwooya@gmail.com>
2022-08-01 10:48:57 -0700

    Add imaging_spatial_analysis

---
+++ R/imaging_spatial_analysis.R
@@ -0,0 +1,38 @@
+# helper functions
+get_coor_from_string <- function(string){
+  temp <- gsub('^.|.$', '', string) # remove the first and last string
+  temp <- strsplit(temp, split=', ')
+  return(c(as.numeric(temp[[1]][1]), as.numeric(temp[[1]][2])))
+}
+
+get_count_from_xy_coor <- function(file, topleft, bottomright){
+  # This function counts cells inside of rectangular box made by the topleft and bottomright xy-coordinates #
+  # Input:
+  # 1. file: _sizes_coordinates.txt
+  # 2. topleft: topleft (x,y) coordiate for a rectangular box
+  # 3. bottomright: bottomright (x,y) coordiate for a rectangular box
+  
+  res <- read.table(file, header=T, sep=',')
+  xmin <- topleft[1]; ymin <- topleft[2]
+  xmax <- bottomright[1]; ymax <- bottomright[2]
+
+  cell_count <- c()
+  for(i in 1:nrow(res)){
+    coor_temp <- res[i, "xy_coordinate"]
+    coor_temp <- get_coor_from_string(string=coor_temp)
+    x_coor <- coor_temp[1]
+    y_coor <- coor_temp[2]
+    x_inside <- y_inside <- FALSE
+    if((xmin < x_coor) & (x_coor < xmax)){x_inside <- TRUE}
+    if((ymin < y_coor) & (y_coor < ymax)){y_inside <- TRUE}
+    if(x_inside & y_inside){
+      cell_count[i] <- TRUE
+    } else {
+      cell_count[i] <- FALSE
+    }
+  }
+  
+  sum(cell_count)
+}
+
+get_count_from_xy_coor(file='/Users/shan/Desktop/M926910_Position1_CD3-BUV395_sizes_coordinates.txt', topleft=c(0,0), bottomright=c(1392,600))
