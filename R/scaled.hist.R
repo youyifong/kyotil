2a577023f53065e9c35eb90d28ece0d28a7c42b3
youyifong <youyifong@gmail.com>
2026-03-14 14:16:33 -0700

    updates

--- R/scaled.hist.R
+++ R/scaled.hist.R
@@ -1,4 +1,4 @@
-scaled.hist = function(dat.ls, scale.factors, bin_width=100, cols=NULL, legend=NULL, cex.legend=1, xlim=NULL, ylim=NULL, span=NULL, ...) {
+scaled.hist = function(dat.ls, scale.factors, bin_width=100, cols=NULL, legend=NULL, cex.legend=1, xlim=NULL, ylim=NULL, span=NULL, add.minus.50=F, ...) {
 
   # dat.ls is a list of lists
   # scale.factors is a list of data frames
@@ -31,20 +31,37 @@ scaled.hist = function(dat.ls, scale.factors, bin_width=100, cols=NULL, legend=N
   # plot histograms
   if (is.null(cols)) cols=1:length(labels)
   for (i in 1:length(labels)) {
+    
     if (is.null(xlim)) xlim=range(breaks)
     if (is.null(ylim)) ylim=range(unlist(lapply(hist_info, function(a) a$counts)))
-    if (i==1) {
-      hist.col = col2rgb(cols[i])
-      hist.col = rgb(hist.col[1], hist.col[2], hist.col[3], alpha=255*.15, maxColorValue=255)
-      plot(hist_info[[i]], freq = TRUE, border="white",  col=hist.col, add=i>1, ylim=ylim, xlim=xlim , ...)
-    }
     
+    # optionally, add -50 to xlim
+    if(add.minus.50) xlim=range(xlim, -50)
+    
+    hist.col = col2rgb(cols[i])
+    # make visible only for the second element of the list
+    if (i!=2) hist.col=col2rgb("white")
+    hist.col = rgb(hist.col[1], hist.col[2], hist.col[3], alpha=255*.15, maxColorValue=255)
+    plot(hist_info[[i]], freq = TRUE, border="white",  col=hist.col, add=i>1, ylim=ylim, xlim=xlim, ...)
+
     if (is.null(span)) {
+      # non-smoothed version
       lines(hist_info[[i]]$mids, hist_info[[i]]$counts, type = "l", col = cols[i], lwd = 2)      
     } else {
       # smoothed version
-      smooth_fit = loess(counts ~ mids, data = data.frame(mids = hist_info[[i]]$mids, counts = hist_info[[i]]$counts), span = span)
+      dat.tmp = data.frame(mids = hist_info[[i]]$mids, counts = hist_info[[i]]$counts)
+      
+      # optionally, add add (-50,0) as the first point
+      if(add.minus.50) {
+        if (!all(hist_info[[i]]$counts==Inf))
+          dat.tmp=rbind(data.frame(mids=-50, counts=0), dat.tmp)
+      }
+      # print(dat.tmp)
+
+      smooth_fit = loess(counts ~ mids, data = dat.tmp, span = span)
       smooth_x = seq(min(hist_info[[i]]$mids), max(hist_info[[i]]$mids), length.out = 500)
+      
+      if (T) smooth_x = c(-50, smooth_x)
       lines(smooth_x, pmax(predict(smooth_fit, newdata = data.frame(mids = smooth_x)), 0), type = "l", col = cols[i], lwd = 2)
     }
 
